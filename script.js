b9b0906da98377fd07203d9b4a9bc70875c063d4
gauravsingh112 <129499661+gauravsingh112@users.noreply.github.com>
2023-07-24 14:24:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
+  if (username === "gaurav" && password === "gaurav123") {
+    // Authentication successful;
+    localStorage.setItem("isLoggedIn", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid credentials. Please try again.");
+  }
+}
